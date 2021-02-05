dcbd4f7b7937c34b6df8eed6b4cd4bec43bd5221
steelerx155 <steelerx155@gmail.com>
2021-02-04 19:55:17 -0500

    created Tasks resource

---
+++ db/migrate/20210205005450_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :content,
+      t.string :category,
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
